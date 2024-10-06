30f9ffc580f65d5414db3b16760b002bf91b9b28
devanshwebhost <devanshrajput032006@gmail.com>
2024-10-06 17:49:36 +0530

    Add files via upload

---
+++ src/anshu.jsx
@@ -0,0 +1,13 @@
+
+let anshu = [
+    {
+        "id": 1,
+        "workName": "Design Website",
+        "description": "Create a design mockup for the new client website.",
+        "dueDate": "2024-10-10",
+        "lastUpdated": "2024-10-01"
+      },
+  
+]
+
+export default  anshu;
---
+++ src/work-update.jsx
@@ -0,0 +1,37 @@
+
+    let workUpdates = [
+        {
+            "id": 1,
+            "name": "devansh",
+            "workName": "Design Website",
+            "description": "Create a design mockup for the new client website.",
+            "dueDate": "2024-10-10",
+            "lastUpdated": "2024-10-01"
+          },
+      {
+        "id": 2,
+        "admin": "Mithilesh",
+        "workName": "Develop Landing Page",
+        "description": "Implement the front-end development of the main landing page.",
+        "dueDate": "2024-10-10",
+        "lastUpdated": "2024-10-01"
+      },
+      {
+        "id": 3,
+        "admin": "Monal",
+        "workName": "Content Writing for Blog",
+        "description": "Write SEO-friendly content for the upcoming blog section.",
+        "dueDate": "2024-10-10",
+        "lastUpdated": "2024-10-01"
+      },
+      {
+        "id": 4,
+        "admin": "Anshu",
+        "workName": "SEO Optimization",
+        "description": "Optimize the website for search engines using best SEO practices.",
+        "dueDate": "2024-10-10",
+        "lastUpdated": "2024-10-01"
+      }
+    ]
+
+    export default  workUpdates;
